0161188e93da7a52a20e88a87015a4fc16a64f38
Michael Fryer <michael.d.fryer@gmail.com>
2018-07-28 23:15:18 +0000

    ba

--- bot.js
+++ bot.js
@@ -1,32 +1,45 @@
 //-------------------------
 //- Node-Library Requires -
 //-------------------------
-const fs = require('fs');
-
+const fs = require('fs'),
+        path = require('path');
 //-----------------------
 //- Other Node Requires -
 //-----------------------
 const Discord = require('discord.js');
-
 //------------------
 //- Local Requires -
 //------------------
-const commands = require('./commands.js');
-
+const options = require('./options.json');
+// This is a var because we update this on change
+var commands = require(`./${options.commandsFile}`);
 //-----------------------
 //- Application Constants
 //-----------------------
-const client = new Discord.Client();
-const prefix = '_';
-
-// Connected message
+const client = new Discord.Client(),
+    prefix = '_',
+    creds = JSON.parse(fs.readFileSync(options.credentialsFile));
+// Commands file watcher
+fs.watch(path.join(__dirname, options.commandsFile), (eventType, filename) => {
+    // Delete the key in the require cache
+    delete require.cache[path.join(__dirname, options.commandsFile)];
+    // Re require the commands
+    commands = require(`./${options.commandsFile}`);
+});
+// Client ready handling
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({ game: { name: "with _help" } });
 });
-
+// Client message handling
 client.on('message', (msg) => {
-    let content = msg.content;
-    let command = content.substr(prefix.length, content.split(' ')[0].length);
+    let content = msg.content,
+            command = content.split(' ')[0];
+
+    if (command.indexOf('_') === 0) {
+        command = command.substring(1);
+        console.log(`Recieved ${command} from ${msg.author}`);
+    }
 
     if (command === 'help') {
         let responseMsg = 'Below is a list of commands followed by a description.\n';
@@ -44,6 +57,25 @@ client.on('message', (msg) => {
         commands[command].action.call(client, msg);
     }
 });
+// Client error handling
+client.on('error', () => {
+    client.clearInterval();
 
+    client.setInterval(() => {
+        client.login(creds.token);
+    }, 5 * 60 * 1000);
+});
+// Client disconnect handling
+client.on('disconnect', () => {
+    client.clearInterval();
+
+    client.setInterval(() => {
+        client.login(creds.token);
+    }, 5 * 60 * 1000);
+});
+// Client ready handling
+client.on('ready', () => {
+    client.clearInterval();
+});
 // Login
-client.login(JSON.parse(fs.readFileSync('creds.json')).token);
+client.login(creds.token);
--- commands.js
+++ commands.js
@@ -1,25 +1,111 @@
 module.exports = {
+    dice: {
+        format: 'dice [ number ]',
+        desc: 'Rolls a dice. Optional faces of 4, 6, 8, 10, 12, or 20 with default of 6.',
+        action(msg) {
+            let content = msg.content,
+                sides = 6,
+                reqSides = content.split(' ')[1],
+                dice = [ 4, 6, 8, 10, 12, 20 ];
+
+            if (reqSides !== -1) {
+                sides = parseInt(reqSides);
+
+                if (!sides || !(dice.includes(sides))) {
+                    msg.reply(`Incorrect format. Please pick one of the following ${dice}.`);
+
+                    return;
+                }
+            }
+
+            msg.reply(`the dice rolled a ${Math.floor(Math.random() * sides) + 1}.`);
+        }
+    },
     hello: {
         format: 'hello',
         desc: 'Says hello back!',
         action(msg) {
-            msg.reply(`Hello ${msg.author}!`);
+            msg.reply(`hello!`);
         }
     },
     invite: {
         format: 'invite',
-        desc: 'Creates an invite',
+        desc: 'Creates an invite to the curren channel.',
         action(msg) {
             if (msg.guild.available) {
-                msg.channel.createInvite().then((invite) => {
-                    msg.reply(`Created invite ${invite.url}`);
+                msg.channel.createInvite()
+                .then((invite) => {
+                    msg.channel.send(`Created invite ${invite.url}`);
                 })
                 .catch(() => {
-                    msg.reply('Unable to crate an invite at this time.');
+                    msg.channel.send('Unable to crate an invite at this time.');
                 });
             }
             else {
-                msg.reply('Unable to create an invite at this time');
+                msg.channel.send('Unable to create an invite at this time.');
+            }
+        }
+    },
+    poll: {
+        format: 'poll "Some message" _option_1 _option_2 ... _option_10',
+        desc: 'Creates a poll with optional message. Only supports 10 options. Signal start of a poll with an underscore, e.g. `_`.',
+        action(msg) {
+            let content = msg.content,
+                splitMsg = content.split('_');
+
+            // have to account for the initial command _poll
+            if (splitMsg.length > 12 || splitMsg.length < 2) {
+                msg.reply('Improper format. Use `_help` to get assistance.');
+
+                return;
+            }
+            else {
+                let channel = msg.channel,
+                    responseMsg = 'New poll created!\n',
+                    pollMsg = content.substring(content.indexOf('"') + 1,
+                            content.lastIndexOf('"')),
+                    // I really hate this
+                    numMap = {
+                        1: 'one',
+                        2: 'two',
+                        3: 'three',
+                        4: 'four',
+                        5: 'five',
+                        6: 'six',
+                        7: 'seven',
+                        8: 'eight',
+                        9: 'nine',
+                        10: 'ten'
+                    },
+                    emojis = this.guilds.get('174283527926448128').emojis;
+
+                if (pollMsg) {
+                    responseMsg = responseMsg + `Poll Message: ${pollMsg}\n`;
+                }
+
+                for (let i = 2; i < splitMsg.length; i++) {
+                    let emoji = emojis.find('name', `hb_${numMap[i - 1]}`);
+                    responseMsg = responseMsg + `\n${emoji} for "${splitMsg[i].trim()}"`;
+                }
+
+                channel.send(responseMsg)
+                .then((message) => {
+                    for (let i = 2; i < splitMsg.length; i++) {
+                        message.react(emojis.find('name', `hb_${numMap[i - 1]}`).id)
+                        .catch(console.error);
+                    }
+
+                    message.createReactionCollector((react, user) => {
+                        return react;
+                    },
+                    {
+                        time: 15 * 1000// 24 * 3600 * 1000
+                    })
+                    .on('end', (collected) => {
+                        console.log(`Got ${collected.size} reacts`);
+                    });
+                })
+                .catch(console.error);
             }
         }
     }
